fd681626d19eba8670004edeb3fd38353cae8810
Dameon Davis <dad1977ut@yahoo.com>
2021-03-13 13:29:15 -0700

    fixing code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = procss.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const notes = require("./db/db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
